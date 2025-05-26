ad138b8d60868c924ad59707d152772acaec1c70
Avril <galimbaabi15@gmail.com>
2025-05-26 19:40:33 +0800

    mdArray.js

---
+++ mdArray.js
@@ -0,0 +1,9 @@
+let names = ["Genevieve", "Juan", "Luna", "Gabriel", "Elise"];
+let ages = [24, 65, 21, 5, 9];
+
+let combined = [];
+for (let i = 0; i < names.length; i++) {
+  combined.push([names[i], ages[i]]);
+}
+
+console.log(combined);
